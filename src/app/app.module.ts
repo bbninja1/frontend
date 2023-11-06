a89d5605cc9445ecfe0ad90815b7fe2f36513896
bbninja1 <shirk.benjamin@gmail.com>
2023-11-06 13:48:27 +0200

    everything frontend added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SignupComponent,
+    LoginComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
